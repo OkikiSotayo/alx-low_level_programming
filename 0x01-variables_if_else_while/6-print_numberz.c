e2e40e01bdea88fcd7c216d38f33f14aabf78c3c
OkikiSotayo <okikisotayo@gmail.com>
2022-09-09 19:46:39 -0700

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,2 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+/**
+ * main - program that prints all single digit numbers of base
+ *
+ * Description: starting from zero followed by a new line
+ * still program that prints
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	putchar(c);
+	putchar('\n');
+
+	return (0);
+}
