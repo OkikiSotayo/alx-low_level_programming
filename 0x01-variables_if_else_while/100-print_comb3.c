a0290873f69fa569e8064e00810eab887586f6a7
OkikiSotayo <okikisotayo@gmail.com>
2022-09-09 20:05:44 -0700

    digit combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that prints all possible different combinations of two digits
+ *
+ * Description: same as main
+ * Same as description
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 100; c++)
+	{
+		putchar ((c / 10) + '0');
+		putchar ((c % 10) + '0');
+
+		if (c != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
