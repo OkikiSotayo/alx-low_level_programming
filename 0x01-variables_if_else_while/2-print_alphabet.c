026c916b2c3e4d5897a9c16b0a9989f618750271
OkikiSotayo <okikisotayo@gmail.com>
2022-09-09 17:08:32 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - another code test
+ *
+ * Description: function for putchar
+ * this code prints putchar command
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
