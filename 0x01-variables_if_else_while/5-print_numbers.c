ca52083940d15e88e465447216639e2f5047eeab
OkikiSotayo <okikisotayo@gmail.com>
2022-09-09 18:16:44 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single digit numbers
+ *
+ * Description: numbers of base starting
+ * program that prints all single numer
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
+
