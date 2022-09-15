3678112f2d69a52263d6d42deef10cd1aeb34d71
OkikiSotayo <okikisotayo@gmail.com>
2022-09-15 16:11:32 -0700

    diagobnal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -8,17 +9,16 @@
 void print_diagonal(int n)
 {
 	int x, i;
-
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
 		for (x = 0; x < n; x++)
 		{
-			for (i = 0; i <= x; i++)
+		for (ii = 0; i <= x; i++)
 			_putchar(' ');
 			_putchar(92);
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
