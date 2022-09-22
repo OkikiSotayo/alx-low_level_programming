834ffb1ef13be72fbd80e3f417bd797dd27fb9a4
OkikiSotayo <okikisotayo@gmail.com>
2022-09-22 07:10:27 -0700

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		length++;
 	}
-	for (i = 0; i < n && src[i] != '\0'; i++ length++)
+	for (i = 0; i < n && src[i] != '\0'; i++, length++)
 	{
 		dest[length] = src[i];
 	}
