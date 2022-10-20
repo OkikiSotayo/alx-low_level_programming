7761fc6c9523e9a59c0f128f0fac419ce7ebbd36
OkikiSotayo <okikisotayo@gmail.com>
2022-10-20 09:35:10 -0700

    print list task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,11 +14,8 @@ size_t print_list(const list_t *h)
 	nodelen = 0;
 	while (h != NULL)
 	{
-		if
-		{
-			(h->str == NULL)
+		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
-		}
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
