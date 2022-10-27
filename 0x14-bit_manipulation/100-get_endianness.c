20e042fbdd5281ea1ec3e4d91cc4278072511ca3
OkikiSotayo <okikisotayo@gmail.com>
2022-10-27 15:26:39 -0700

    get endianness task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_endianness - function that checks the endianness
+ *
+ * Return: 0 or 1 based on size
+ *
+ */
+
+int get_endianness(void)
+{
+	unsigned int a;
+	char *c;
+
+	a = 1;
+	c = (char *) &x;
+
+	return ((int)*c);
+}
